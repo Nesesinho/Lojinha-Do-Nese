cded250aa75775f85c64943b61276b28218272a5
Miguel-code-maker <Miguel.Siqueira.Reis@gmail.com>
2021-03-27 21:57:59 -0300

    consertando o javaScript

--- js/ItensController.js
+++ js/ItensController.js
@@ -1,71 +1,77 @@
 import Service from './Service.js';
 
 export default class ItensController {
-  constructor() {
-    this.money = 0;
-    this.placeItens = document.querySelector('.lista');
-    this.itens = Service.getAll('url')
-    this.setInterval = [];
+    constructor() {
+        this.money = 0;
+        this.placeItens = document.querySelector('.itens__list');
+        this.itens = Service.getAll('url')
+        this.setInterval = [];
 
-    this.init();
-  }
+        this.init();
+    }
 
-  init() {
-    this.createItensHtml();
-    const btnComprar = document.querySelectorAll('.btn_comprar')
-    btnComprar.forEach(btn => {
-      btn.addEventListener('click', e => {
-        e.preventDefault()
-        const id = btn.dataset.id;
-        this.comprar(id);
-      })
-    })
-    this.render();
-  }
+    init() {
+        this.createItensHtml();
+        const btnComprar = document.querySelectorAll('.btn_comprar')
+        btnComprar.forEach(btn => {
+            btn.addEventListener('click', e => {
+                e.preventDefault()
+                const id = btn.dataset.id;
+                this.comprar(id);
+            })
+        })
+        this.render();
+    }
 
-  createItensHtml() {
-    this.placeItens.innerHTML = '';
-    this.itens.forEach(iten => {
-      const template = `
-        <li>
-          <p>${iten.nome}</p>
-          <p>${iten.quantidade}</p>
-          <button type="button" class="btn_comprar" data-id="${iten.id}">comprar</button>
-          <p>${iten.preco.toFixed(0)}</p>
+    createItensHtml() {
+        this.placeItens.innerHTML = '';
+        this.itens.forEach(iten => {
+            const template = `
+        <li class="itens__list__iten">
+          <div class="itens__list__iten__picture">
+            <img src="${iten.picture}">
+          </div>
+          <div class="itens__list__iten__desc">
+            <p class="itens__list__iten__nome">${iten.nome}</p>
+            <p class="itens__list__iten__quantidade">Quantidade: ${iten.quantidade}</p>
+            <p class="itens__list__iten_preco">Preço: ${iten.preco.toFixed(0)} R$</p>
+            <p class="itens__list__iten__renda">Renda/m: ${iten.renda} R$</p>
+            <button type="button" class="btn_comprar" data-id="${iten.id}">comprar</button>
+          </div>
         </li>
       `;
-      this.placeItens.innerHTML += template;
-    })
-  }
-
-  comprar(id) {
-    const iten = this.itens.find(iten => iten.id == id);
-    const index = this.itens.indexOf(iten);
-    if (!(iten && this.money >= iten.preco)) return window.alert('não tem dinheiro suficiente')
-    if (iten.nome == 'douglas' && iten.quantidade == 99) {
-      alert('hum pelo visto alguém quer fazer um harem.')
+            this.placeItens.innerHTML += template;
+        })
     }
-    this.money -= iten.preco;
-    iten.preco += iten.preco * (25 / 100);
-    iten.quantidade++;
-    this.itens[index] = iten;
-    this.init();
-  }
 
-  render() {
-    this.setInterval.forEach(interval => {
-      clearInterval(interval);
-    })
-    this.itens.forEach(iten => {
-      const getMoney = iten.quantidade > 0? iten.renda*iten.quantidade:0;
-      this.setInterval.push(
-          setInterval(() => {
-          this.money += getMoney;
-          console.log(getMoney)
-          document.querySelector('.dinheiro').textContent = this.money.toFixed(0)+' R$';
+    comprar(id) {
+        const iten = this.itens.find(iten => iten.id == id);
+        const index = this.itens.indexOf(iten);
+        if (!(iten && this.money >= iten.preco)) return window.alert('não tem dinheiro suficiente')
+        if (iten.nome == 'douglas' && iten.quantidade == 99) {
+            alert('hum pelo visto alguém quer fazer um harem.')
+        }
+        this.money -= iten.preco;
+        iten.preco += iten.preco * (25 / 100);
+        iten.quantidade++;
+        this.itens[index] = iten;
+        this.init();
+    }
 
-        }, 1000)
-      )
-    })
-  }
+    render() {
+        this.setInterval.forEach(interval => {
+            clearInterval(interval);
+        })
+        this.itens.forEach(iten => {
+            const getMoney = iten.quantidade > 0? iten.renda*iten.quantidade:0;
+            this.setInterval.push(
+                setInterval(() => {
+                    this.money += getMoney;
+                    console.log(getMoney)
+                    document.querySelector('.dinheiro').textContent = this.money.toFixed(0)+' R$';
+
+                }, 1000)
+            )
+        })
+    }
 }
--- js/Service.js
+++ js/Service.js
@@ -1,67 +1,75 @@
 export default class Service {
-  constructor() {   
-      throw new Error('Esta classe não pode ser instanciada');
-  }
+    constructor() {
+        throw new Error('Esta classe não pode ser instanciada');
+    }
 
-  static getAll(url) {
-    const itens = [
-      {
-        id: 1,
-        nome: "stonks",
-        preco: 1000,
-        renda: 50,
-        quantidade: 1
-      },
-      {
-        id: 2,
-        nome: "rafa",
-        preco: 5000, 
-        renda: 250,
-        quantidade: 0
-      },
-      {
-        id:3,
-        nome: 'juan',
-        preco: 10000,
-        renda: 500,
-        quantidade: 0
-      },
-      {
-        id: 4,
-        nome: "coelho",
-        preco: 25000,
-        renda: 1250,
-        quantidade: 0
-      },
-      {
-        id: 5,
-        nome: "mingal",
-        preco: 50000,
-        renda: 2500,
-        quantidade: 0
-      },
-      {
-        id: 6,
-        nome: "douglas",
-        preco: 100000,
-        renda: 5000,
-        quantidade: 0
-      },
-      {
-        id: 8,
-        nome: "cellbit",
-        preco: 500000,
-        renda: 25000,
-        quantidade: 0
-      },
-      {
-        id: 9,
-        nome: "jose",
-        preco: 1000000,
-        renda: 50000,
-        quantidade: 0
-      }
-    ]
-    return itens
-  }
+    static getAll(url) {
+        const itens = [
+            {
+                id: 1,
+                picture: 'https://cdn.discordapp.com/avatars/693670430829051985/bfb9a309f9c54f4e6979e49131f91cc9.png',
+                nome: "Stonks",
+                preco: 1000,
+                renda: 50,
+                quantidade: 1
+            },
+            {
+                id: 2,
+                picture: 'https://cdn.discordapp.com/avatars/693670430829051985/bfb9a309f9c54f4e6979e49131f91cc9.png',
+                nome: "Rafa",
+                preco: 5000,
+                renda: 250,
+                quantidade: 0
+            },
+            {
+                id:3,
+                picture: 'https://cdn.discordapp.com/avatars/693670430829051985/bfb9a309f9c54f4e6979e49131f91cc9.png',
+                nome: 'Juan',
+                preco: 10000,
+                renda: 500,
+                quantidade: 0
+            },
+            {
+                id: 4,
+                picture: 'https://cdn.discordapp.com/avatars/693670430829051985/bfb9a309f9c54f4e6979e49131f91cc9.png',
+                nome: "Coelho",
+                preco: 25000,
+                renda: 1250,
+                quantidade: 0
+            },
+            {
+                id: 5,
+                picture: 'https://cdn.discordapp.com/avatars/693670430829051985/bfb9a309f9c54f4e6979e49131f91cc9.png',
+                nome: "Mingal",
+                preco: 50000,
+                renda: 2500,
+                quantidade: 0
+            },
+            {
+                id: 6,
+                picture: 'https://cdn.discordapp.com/avatars/693670430829051985/bfb9a309f9c54f4e6979e49131f91cc9.png',
+                nome: "Douglas",
+                preco: 100000,
+                renda: 5000,
+                quantidade: 0
+            },
+            {
+                id: 8,
+                picture: 'https://cdn.discordapp.com/avatars/693670430829051985/bfb9a309f9c54f4e6979e49131f91cc9.png',
+                nome: "Cellbit",
+                preco: 500000,
+                renda: 25000,
+                quantidade: 0
+            },
+            {
+                id: 9,
+                picture: 'https://cdn.discordapp.com/avatars/693670430829051985/bfb9a309f9c54f4e6979e49131f91cc9.png',
+                nome: "Jose",
+                preco: 1000000,
+                renda: 50000,
+                quantidade: 0
+            }
+        ]
+        return itens
+    }
 }
