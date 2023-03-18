718e70059b55e363b1e938a4d8e90450393fcbbb
Nese <70600721+Nesesinho@users.noreply.github.com>
2023-03-18 01:53:19 -0300

    mudando fotinhas.

--- js/Service.js
+++ js/Service.js
@@ -7,7 +7,7 @@ export default class Service {
         const itens = [
             {
                 id: 1,
-                picture: 'https://images-ext-2.discordapp.net/external/PIaL9IFiVRJG28_eZaA9A1tRRQacWK5fyTnubdbVuL8/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/752351959872438302/83930bbccf8e3cd42a7858c0bf5e62d9.png',
+                picture: 'https://cdn.discordapp.com/avatars/752351959872438302/f51435d56b2cb05b190187cf73d51c9a.png',
                 nome: "Stonks",
                 preco: 1000,
                 renda: 10,
@@ -15,7 +15,7 @@ export default class Service {
             },
             {
                 id: 2,
-                picture: 'https://images-ext-2.discordapp.net/external/qohOLfcHK-AR6Jn9_wxArs2R5MTmDCnXUjlZ0442EVY/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/808088684921618433/32ecaea05813c52a53968965aa4d75c2.png',
+                picture: 'https://images-ext-1.discordapp.net/external/fruaCfNCrULw5SKlUYrW1WlYx1zRa6b4Jhb7-FxJof0/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/1062079734621679646/6c5996770c985bcd6e5b68131ff2ba04.png',
                 nome: "Rafa",
                 preco: 5000,
                 renda: 50,
@@ -23,7 +23,7 @@ export default class Service {
             },
             {
                 id:3,
-                picture: 'https://images-ext-1.discordapp.net/external/thPxQkDuykDr85y8-fHkRUOC0EIYAaqRer5BF63nXk8/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/443797355393974272/0f5559a2b9d0b30e3a0f5ac95d0521a4.png',
+                picture: 'https://images-ext-2.discordapp.net/external/QaxZQepNoooN2Ch3-zG5OxJ9VpiY7crQEzjFhxdivRI/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/443797355393974272/b4ddb336627d7266fb24b84eb8bb2c4f.png',
                 nome: 'Juan',
                 preco: 10000,
                 renda: 100,
@@ -31,7 +31,7 @@ export default class Service {
             },
             {
                 id: 4,
-                picture: 'https://images-ext-2.discordapp.net/external/xavUuQEO3LVQjOKpIzOELfzgBd_zhJVbBVpMI06On9M/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/496471981479428107/3381e71b34f4bd397a250d44396dc75c.png',
+                picture: 'https://cdn.discordapp.com/avatars/496471981479428107/06742033f09f6086fbef490a2a9bccf5.png',
                 nome: "Coelho",
                 preco: 25000,
                 renda: 250,
@@ -39,7 +39,7 @@ export default class Service {
             },
             {
                 id: 5,
-                picture: 'https://images-ext-1.discordapp.net/external/q75DwoVyLWBAi816ExSpCqniygDLx-TqpuZrlSCVp6g/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/488705510430736384/7e0fbb894b5b604adf935f951ad593ac.png',
+                picture: 'https://images-ext-2.discordapp.net/external/Knwcy1ILW4wBnNQbI7jEcZ5WYkW_AfTpx-hiU8SJaf4/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/488705510430736384/97c4712b82b3339b6bc057a51c849a35.png',
                 nome: "Arthur",
                 preco: 38000,
                 renda: 380,
@@ -47,7 +47,7 @@ export default class Service {
             },
             {
                 id: 6,
-                picture: 'https://images-ext-1.discordapp.net/external/C_bIHW66dpfbw427AiI5eyqvWvOlKcDHOtm4X_CeEAQ/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/693670430829051985/ac7aed756a31dc2823cfc3dee47a906a.png',
+                picture: 'https://images-ext-1.discordapp.net/external/Og-AW1lN3xXa42EcFKvdXuAOoF0Ffpwh2rpZr1cz2wA/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/693670430829051985/21f511509f6e2c32970c411945e510db.png',
                 nome: "Mingal",
                 preco: 50000,
                 renda: 500,
@@ -55,7 +55,7 @@ export default class Service {
             },
             {
                 id: 7,
-                picture: 'https://images-ext-1.discordapp.net/external/nHTIERnPA5T6ZqYLycFX2L943LO9BUZBcAfxVA-VDIM/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/371823685142708244/7c598d1c42d215f19a15dea4cd6f3edb.png',
+                picture: 'https://cdn.discordapp.com/avatars/371823685142708244/71133590582b07e7f417e719416314f1.png',
                 nome: "Douglas",
                 preco: 100000,
                 renda: 1000,
@@ -63,7 +63,7 @@ export default class Service {
             },
             {
                 id: 8,
-                picture: 'https://images-ext-2.discordapp.net/external/QiF4Hfl2P5JG_ywiixT82Vv7tIayE-m6b_YGSLXfg5Q/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/369279967374213120/e05762cc5314c908153f4e5a68a81d61.png',
+                picture: 'https://cdn.discordapp.com/avatars/369279967374213120/742d46dc6f74fd4c135ef0ac319e4f9f.png',
                 nome: "Cellbit",
                 preco: 500000,
                 renda: 5000,
@@ -71,7 +71,7 @@ export default class Service {
             },
             {
                 id: 9,
-                picture: 'https://cdn.discordapp.com/avatars/255752076338921472/0670bcd5e7a9ec40ead62360fd33a664.png',
+                picture: 'https://images-ext-2.discordapp.net/external/i5gakaMry9IV4nZDMtW3XpmAg2YfQm5EWI55VJZxWvQ/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/255752076338921472/c57219e2b1ef8209ac39d782f8e4482f.png',
                 nome: 'Nese',
                 preco: 750000,
                 renda: 7500,
